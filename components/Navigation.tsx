387b9916e8a3ec025c32b79092d315ce0e18f132
Alex <forcablaugrana@gmail.com>
2022-09-29 18:37:16 +0300

    first commit

---
+++ components/Navigation.tsx
@@ -0,0 +1,9 @@
+export default function Navigation() {
+  return (
+    <nav class="flex h-[50] absolute top-0 left-0 right-0 bg-blue-400 text-white items-center justify-between px-4">
+      <h1 class="text-lg">
+        Fresh-App
+      </h1>
+    </nav>
+  );
+}
